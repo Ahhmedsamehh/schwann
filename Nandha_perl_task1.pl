8fc13e47a51de2c2fad2304f5f8976aa796ffde2
K J Nandha kishore <65210990+Nandhakishore97@users.noreply.github.com>
2021-08-07 08:33:21 +0530

    Update Nandha_perl_task1.pl

--- Nandha_perl_task1.pl
+++ Nandha_perl_task1.pl
@@ -1,9 +1,48 @@
-#!usr/bin/perl
+#!/usr/bin/perl
 
 $name ="KJ Nandha Kishore";
 $email = "nandhakishorekj97@gmail.com";
-$slack_usrname = "@ Nandha";
+$slack_usrname = "\@Nandha";
+$bio_stack = "Genomics";
+$twit_handle = "\@nandha_kj";
 
-print "\n*************************************************\n";
-print("\tName            :  $name\n\tE-mail          :  $email\n\tSlack user name :  $slack_usrname");
-print "\n*************************************************\n";
+#Here both the strings are not equal for calculating the hamming distance
+
+sub hamDist
+	{
+	  	$fname = $_[0];
+		$sname = $_[1];
+		
+		if (length($fname) <length($sname))
+			{
+			    $f_name = $sname;
+			    $s_name = $fname;
+			}
+		else
+			{
+	                   $f_name = $fname;
+			   $s_name = $sname;
+			}
+		@ar = split('',$f_name);
+		@arr = split('',$s_name);
+
+		for($x=0; $x<length($f_name); $x++)
+			{
+		
+			   if($ar[$x] ne $arr[$x])
+				{
+				   $count+=1;
+				
+				} 
+			   else
+				{
+				   $count+=0;
+				}
+			}
+	
+		return $count;
+	}
+
+$ham_dist = hamDist($slack_usrname, $twit_handle);
+print("\n\tName              :  $name\n\tE-mail            :  $email\n\tSlack user name   :  $slack_usrname\n\tBioStack          :  $bio_stack\n\tTwitter handle    :  $twit_handle\n\tHamming distance  :  $ham_dist\n");
+
